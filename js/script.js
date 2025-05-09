c8fe58ae67f11bb2940df8364decad2f4f75ff61
git-oberoi <shivamraj91150@gmail.com>
2025-05-09 23:04:51 +0530

    Blog-Page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+/*Hamburger Button For Small screen */
+
 document.querySelector('.hamburger').addEventListener('click', function() {
   document.querySelector('.main-nav').classList.toggle('active');
 });
+
+/*Read More Button for Blog Page*/
+
+console.log("Script loaded!");  // This should appear in the console.
+$(document).ready(function () {
+  $('.read-more').click(function () {
+    const post = $(this).siblings('.full-post');
+    const button = $(this);
+
+    post.slideToggle(300, function () {
+      const isVisible = post.is(':visible');
+      
+      if (isVisible) {
+        button.text('Read Less');
+      } else {
+        button.text('Read More');
+      }
+    });
+  });
+});
+
